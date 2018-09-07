d1c547bc554ac4348dcd3ba2129f0cb388b0bfb6
Julio <julio@beautyhall.com.br>
2018-09-06 23:18:46 -0300

    Criação de um novo componente

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import Simples from './componentes/Simples'
 
 class App extends Component {
 	render(){
 		return (
 			<View style={styles.container}>
-				<Text style={styles.f20}>App!</Text>
+				<Simples texto='Flexivel!!' />
 			</View>
 		)
 	}
@@ -16,9 +17,6 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center',
-	},
-	f20: {
-		fontSize: 40
 	}
 })
 
