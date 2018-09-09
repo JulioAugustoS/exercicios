5c72a53f22bd3b7e108ea5cbb11c64da36854c7b
Julio <julio@beautyhall.com.br>
2018-09-08 23:52:54 -0300

    Validar Props e Eventoss Controlados

--- src/Menu.js
+++ src/Menu.js
@@ -6,8 +6,16 @@ import ParImpar from './componentes/ParImpar'
 import Inverter, { MegaSena } from './componentes/Multi'
 import Contador from './componentes/Contador'
 import Plataformas from './componentes/Plataformas'
+import ValidarProps from './componentes/ValidarProps'
+import Evento from './componentes/Evento'
 
 export default createDrawerNavigator({
+    Evento: {
+        screen: Evento
+    },
+    ValidarProps: {
+        screen: () => <ValidarProps label="Teste" ano={18} />
+    },
     Plataformas: {
         screen: Plataformas
     },
